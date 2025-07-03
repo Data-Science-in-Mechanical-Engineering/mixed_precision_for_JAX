d675856f4a2a010dedf8f16a538684efcd7c5391
Alexander Graefe <alexander.graefe@dsme.rwth-aachen.de>
2025-07-03 15:47:40 +0200

    changed workflow again

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 import sys
 from pathlib import Path
 
-# sys.path.insert(0, str(Path('..').resolve()))
+sys.path.insert(0, str(Path('..').resolve()))
 
 project = 'MPX'
 copyright = '2025, Alexander Gräfe'
--- docs/modules.rst
+++ docs/modules.rst
@@ -1,5 +1,5 @@
-mpx
-===
+mixed_precision_for_JAX
+=======================
 
 .. toctree::
    :maxdepth: 4
