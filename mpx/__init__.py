7d6b6eb8b2be652fa566991bc43cbd2ff1602388
Alexander Graefe <alexander.graefe@dsme.rwth-aachen.de>
2025-07-14 12:55:50 +0200

    fixed bug in lazy import of constants

--- mpx/__init__.py
+++ mpx/__init__.py
@@ -2,7 +2,7 @@
 Mixed Precision for JAX - A library for mixed precision training in JAX
 """
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 from ._cast import (
     cast_tree,
@@ -14,9 +14,26 @@ from ._cast import (
     force_full_precision,
     cast_function,
 )
-from ._dtypes import half_precision_datatype, set_half_precision_datatype, FLOAT16_MAX, BFLOAT16_MAX, HALF_PRECISION_DATATYPE
+from ._dtypes import half_precision_datatype, set_half_precision_datatype, HALF_PRECISION_DATATYPE  # , FLOAT16_MAX, BFLOAT16_MAX
 from ._loss_scaling import DynamicLossScaling, all_finite, scaled
 from ._grad_tools import select_tree, filter_grad, filter_value_and_grad, optimizer_update, calculate_scaled_grad
+
+import sys
+import types
+
+import jax.numpy as jnp
+# We do to avoid that jax is directly called when importing this module.
+# This is to ensure that the constants are lazily initialized.
+class _MaxConstantsLazyInit(types.ModuleType):    
+    @property
+    def FLOAT16_MAX(self):
+        return jnp.ones([], dtype=jnp.float32) * (2 - 2**(-10)) * 2**15
+    
+    @property
+    def BFLOAT16_MAX(self):
+        return jnp.array([((2**8 - 1) * 2**(120))], dtype=jnp.float32)[0]
+
+sys.modules[__name__].__class__ = _MaxConstantsLazyInit
 
 __all__ = [
     # Cast functions
--- mpx/_dtypes.py
+++ mpx/_dtypes.py
@@ -1,22 +1,6 @@
 import jax.numpy as jnp
 
-import sys
-import types
-
 HALF_PRECISION_DATATYPE = jnp.float16  # Default half precision datatype
-
-# We do to avoid that jax is directly called when importing this module.
-# This is to ensure that the constants are lazily initialized.
-class _MaxConstantsLazyInit(types.ModuleType):    
-    @property
-    def FLOAT16_MAX(self):
-        return jnp.ones([], dtype=jnp.float32) * (2 - 2**(-10)) * 2**15
-    
-    @property
-    def BFLOAT16_MAX(self):
-        return jnp.array([((2**8 - 1) * 2**(120))], dtype=jnp.float32)[0]
-
-sys.modules[__name__].__class__ = _MaxConstantsLazyInit
 
 def set_half_precision_datatype(datatype):
     """
